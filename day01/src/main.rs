c67badce2e512c5f1136a7f0f8aeb4e7472b12d4
Nathan Goldbaum <nathan12343@gmail.com>
2019-12-02 09:32:28 -0700

    day 1 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -7,10 +7,24 @@ type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 fn main() -> Result<()> {
     let contents = get_contents("input");
 
-    let mut total_fuel: i64 = 0;
+    let mut fuel_requirements: Vec<i64> = Vec::new();
 
     for line in contents.lines() {
-        total_fuel += fuel_required(line.parse::<i64>()?);
+        fuel_requirements.push(line.parse::<i64>()?);
+    }
+
+    let mut total_fuel: i64 = 0;
+
+    for req in &fuel_requirements {
+        total_fuel += fuel_required(*req).unwrap();
+    }
+
+    dbg!(total_fuel);
+
+    let mut total_fuel: i64 = 0;
+
+    for req in &fuel_requirements {
+        total_fuel += fuel_required_with_fuel(*req)
     }
 
     dbg!(total_fuel);
@@ -18,9 +32,23 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn fuel_required(mass: i64) -> i64 {
+fn fuel_required(mass: i64) -> Option<i64> {
     let third_mass: f64 = (mass as f64) / 3.0;
-    return (third_mass.floor() - 2.0) as i64;
+    let ret = (third_mass.floor() - 2.0) as i64;
+    if ret >= 0 {
+        return Some(ret);
+    }
+    return None;
+}
+
+fn fuel_required_with_fuel(mass: i64) -> i64 {
+    let mut fuel = fuel_required(mass).unwrap();
+    let mut total_fuel = fuel;
+    while let Some(additional_fuel) = fuel_required(fuel) {
+        total_fuel += additional_fuel;
+        fuel = additional_fuel;
+    }
+    total_fuel
 }
 
 fn get_contents(filename: &str) -> String {
@@ -39,9 +67,16 @@ mod tests {
 
     #[test]
     fn part1() {
-        assert!(fuel_required(12) == 2);
-        assert!(fuel_required(14) == 2);
-        assert!(fuel_required(1969) == 654);
-        assert!(fuel_required(100756) == 33583);
+        assert!(fuel_required(12) == Some(2));
+        assert!(fuel_required(14) == Some(2));
+        assert!(fuel_required(1969) == Some(654));
+        assert!(fuel_required(100756) == Some(33583));
+    }
+
+    #[test]
+    fn part2() {
+        assert!(fuel_required_with_fuel(14) == 2);
+        assert!(fuel_required_with_fuel(1969) == 966);
+        assert!(fuel_required_with_fuel(100756) == 50346);
     }
 }
