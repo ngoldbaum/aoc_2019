1700eeaf25067243b8ea6b8d9c865b9b8726324e
Nathan Goldbaum <nathan12343@gmail.com>
2019-12-03 11:21:44 -0700

    day 3 part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use std::error;
 use std::fs::File;
 use std::io::Read;
@@ -20,18 +19,24 @@ fn get_min_distance(contents: &str) -> usize {
 
     let point_sets = walk_instructions(instructions);
 
-    point_sets
-        .0
-        .intersection(&point_sets.1)
-        .map(|p| (p.x.abs() + p.y.abs()) as usize)
-        .min()
-        .unwrap()
+    let mut intersections: Vec<usize> = Vec::new();
+
+    for p0 in &point_sets.0 {
+        for p1 in &point_sets.1 {
+            if (p0.x == p1.x) && (p0.y == p1.y) {
+                intersections.push(p0.step_count + p1.step_count);
+            }
+        }
+    }
+
+    *intersections.iter().min().unwrap()
 }
 
 #[derive(Clone, Debug, Hash, Eq, PartialEq)]
 struct Point {
     x: i64,
     y: i64,
+    step_count: usize,
 }
 
 #[derive(Clone, Debug)]
@@ -74,20 +79,19 @@ fn interpret_contents(contents: &str) -> (Vec<Instruction>, Vec<Instruction>) {
 
 fn walk_instructions(
     instructions: (Vec<Instruction>, Vec<Instruction>),
-) -> (HashSet<Point>, HashSet<Point>) {
+) -> (Vec<Point>, Vec<Point>) {
     (
         walk_instruction_vec(instructions.0),
         walk_instruction_vec(instructions.1),
     )
 }
 
-fn walk_instruction_vec(instruction_vec: Vec<Instruction>) -> HashSet<Point> {
-    let mut result: HashSet<Point> = HashSet::new();
+fn walk_instruction_vec(instruction_vec: Vec<Instruction>) -> Vec<Point> {
+    let mut result: Vec<Point> = Vec::new();
 
     let mut x = 0;
     let mut y = 0;
-
-    result.insert(Point { x, y });
+    let mut step_count = 0;
 
     for instruction in instruction_vec {
         for _ in 0..instruction.step_size {
@@ -97,11 +101,12 @@ fn walk_instruction_vec(instruction_vec: Vec<Instruction>) -> HashSet<Point> {
                 Direction::Left => x -= 1,
                 Direction::Right => x += 1,
             }
-            result.insert(Point { x, y });
+            step_count += 1;
+            result.push(Point { x, y, step_count });
         }
     }
 
-    result.remove(&Point { x: 0, y: 0 });
+    result.retain(|p| !(p.x == 0 && p.y == 0));
 
     result
 }
@@ -121,16 +126,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part1() {
-        assert!(get_min_distance("R8,U5,L5,D3\nU7,R6,D4,L4") == 6);
+    fn part2() {
+        assert!(get_min_distance("R8,U5,L5,D3\nU7,R6,D4,L4") == 30);
         assert!(
             get_min_distance("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83")
-                == 159
+                == 610
         );
         assert!(
             get_min_distance(
                 "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
-            ) == 135
+            ) == 410
         );
     }
 }
